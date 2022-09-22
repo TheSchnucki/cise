fd423ca35387022511d93f80244df06164c5f3d4
Mark Schneider <mark.schneider@gmx.net>
2022-09-22 14:57:11 +0200

    -s

--- scripts/cise.js
+++ scripts/cise.js
@@ -1,21 +1,21 @@
 Hooks.on('diceSoNiceReady', (dice3d) => {
 
     dice3d.addSystem({
-        id: "scriblemotions",
-        name: "⁂ Scriblemotions (d6, special)"
+        id: "scriblemotion",
+        name: "⁂ Scriblemotion (d6, special)"
     }, false);
 
     dice3d.addDicePreset({
         type: "d6",
         labels: [
-            'modules/cise/artwork/scriblemotions/d6-1.webp',
-            'modules/cise/artwork/scriblemotions/d6-2.webp',
-            'modules/cise/artwork/scriblemotions/d6-3.webp',
-            'modules/cise/artwork/scriblemotions/d6-4.webp',
-            'modules/cise/artwork/scriblemotions/d6-5.webp',
-            'modules/cise/artwork/scriblemotions/d6-6.webp'
+            'modules/cise/artwork/scriblemotion/d6-1.webp',
+            'modules/cise/artwork/scriblemotion/d6-2.webp',
+            'modules/cise/artwork/scriblemotion/d6-3.webp',
+            'modules/cise/artwork/scriblemotion/d6-4.webp',
+            'modules/cise/artwork/scriblemotion/d6-5.webp',
+            'modules/cise/artwork/scriblemotion/d6-6.webp'
         ],
-        system: "scriblemotions"
+        system: "scriblemotion"
     });
 
     dice3d.addSystem({
