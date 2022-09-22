ea19cd69d71bd4fe08ce56018af1123b8b847a61
Mark Schneider <mark.schneider@gmx.net>
2022-09-22 15:02:13 +0200

    Update cise.js

--- scripts/cise.js
+++ scripts/cise.js
@@ -8,12 +8,12 @@ Hooks.on('diceSoNiceReady', (dice3d) => {
     dice3d.addDicePreset({
         type: "d6",
         labels: [
-            'modules/cise/artwork/scriblemotion/d6-1.webp',
-            'modules/cise/artwork/scriblemotion/d6-2.webp',
-            'modules/cise/artwork/scriblemotion/d6-3.webp',
-            'modules/cise/artwork/scriblemotion/d6-4.webp',
-            'modules/cise/artwork/scriblemotion/d6-5.webp',
-            'modules/cise/artwork/scriblemotion/d6-6.webp'
+            'modules/cise/artwork/cisepunk/d6-1.webp',
+            'modules/cise/artwork/cisepunk/d6-2.webp',
+            'modules/cise/artwork/cisepunk/d6-3.webp',
+            'modules/cise/artwork/cisepunk/d6-4.webp',
+            'modules/cise/artwork/cisepunk/d6-5.webp',
+            'modules/cise/artwork/cisepunk/d6-6.webp'
         ],
         system: "scriblemotion"
     });
