31219c608f071414a8bb056565d33d76307d6bb2
Mark Schneider <mark.schneider@gmx.net>
2022-09-22 14:19:40 +0200

    add own artwork

--- scripts/cise.js
+++ scripts/cise.js
@@ -17,4 +17,22 @@ Hooks.on('diceSoNiceReady', (dice3d) => {
         ],
         system: "scriblemotions"
     });
+
+    dice3d.addSystem({
+        id: "cisepunk",
+        name: "CisePunk (d6)"
+    }, false);
+
+    dice3d.addDicePreset({
+        type: "d6",
+        labels: [
+            'modules/cise/artwork/cisepunk/d6-1.webp',
+            'modules/cise/artwork/cisepunk/d6-2.webp',
+            'modules/cise/artwork/cisepunk/d6-3.webp',
+            'modules/cise/artwork/cisepunk/d6-4.webp',
+            'modules/cise/artwork/cisepunk/d6-5.webp',
+            'modules/cise/artwork/cisepunk/d6-6.webp'
+        ],
+        system: "scriblemotions"
+    });
 });
