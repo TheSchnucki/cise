cc7218c890f1df930e60605e4768db077b7610db
Mark Schneider <mark.schneider@gmx.net>
2022-09-22 15:14:20 +0200

    reverted to 0.1.3

--- scripts/cise.js
+++ scripts/cise.js
@@ -1,8 +1,26 @@
 Hooks.on('diceSoNiceReady', (dice3d) => {
 
     dice3d.addSystem({
-        id: "dork",
-        name: "⁂ Dork (d6, special)"
+        id: "scriblemotion",
+        name: "⁂ Scriblemotion (d6, special)"
+    }, false);
+
+    dice3d.addDicePreset({
+        type: "d6",
+        labels: [
+            'modules/cise/artwork/scriblemotion/d6-1.webp',
+            'modules/cise/artwork/scriblemotion/d6-2.webp',
+            'modules/cise/artwork/scriblemotion/d6-3.webp',
+            'modules/cise/artwork/scriblemotion/d6-4.webp',
+            'modules/cise/artwork/scriblemotion/d6-5.webp',
+            'modules/cise/artwork/scriblemotion/d6-6.webp'
+        ],
+        system: "scriblemotion"
+    });
+
+    dice3d.addSystem({
+        id: "cisepunk",
+        name: "⁂ cisepunk (d6, special)"
     }, false);
 
     dice3d.addDicePreset({
@@ -15,6 +33,6 @@ Hooks.on('diceSoNiceReady', (dice3d) => {
             'modules/cise/artwork/cisepunk/d6-5.webp',
             'modules/cise/artwork/cisepunk/d6-6.webp'
         ],
-        system: "dork"
+        system: "cisepunk"
     });
 });
