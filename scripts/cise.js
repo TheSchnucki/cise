b5d9602114ea553d9f6f56df7ad9561bf1d8f330
Mark Schneider <mark.schneider@gmx.net>
2022-09-23 08:47:55 +0200

    add cybercise-black

--- scripts/cise.js
+++ scripts/cise.js
@@ -1,21 +1,29 @@
 Hooks.on('diceSoNiceReady', (dice3d) => {
 
     dice3d.addSystem({
-        id: "scriblemotion",
-        name: "⁂ Scriblemotion (d6, special)"
+        id: "cybercise-black",
+        name: "⁂ Cybercise-Black (d6, special)"
     }, false);
 
     dice3d.addDicePreset({
         type: "d6",
         labels: [
-            'modules/cise/artwork/scriblemotion/d6-1.webp',
-            'modules/cise/artwork/scriblemotion/d6-2.webp',
-            'modules/cise/artwork/scriblemotion/d6-3.webp',
-            'modules/cise/artwork/scriblemotion/d6-4.webp',
-            'modules/cise/artwork/scriblemotion/d6-5.webp',
-            'modules/cise/artwork/scriblemotion/d6-6.webp'
+            'modules/cise/artwork/cybercise-black/d6-1.webp',
+            'modules/cise/artwork/cybercise-black/d6-2.webp',
+            'modules/cise/artwork/cybercise-black/d6-3.webp',
+            'modules/cise/artwork/cybercise-black/d6-4.webp',
+            'modules/cise/artwork/cybercise-black/d6-5.webp',
+            'modules/cise/artwork/cybercise-black/d6-6.webp'
         ],
-        system: "scriblemotion"
+        bumpMaps:[
+          'modules/cise/artwork/cybercise-black/d6-1-bump.webp',
+          'modules/cise/artwork/cybercise-black/d6-2-bump.webp',
+          'modules/cise/artwork/cybercise-black/d6-3-bump.webp',
+          'modules/cise/artwork/cybercise-black/d6-4-bump.webp',
+          'modules/cise/artwork/cybercise-black/d6-5-bump.webp',
+          'modules/cise/artwork/cybercise-black/d6-6-bump.webp'
+      ],
+        system: "cybercise-black"
     });
 
     dice3d.addSystem({id:"scrible",
